3c00cd8cc22a6114c928dfad57d14d2fef67fb46
fa <fa@ford.com>
2024-11-17 23:39:35 +0530

    fixing issues

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './app'
+import App from './App'
 import { ContextProvider } from './components/SocketContext'
 import './init'
 
